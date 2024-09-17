71f2649a1f1e089999ef1d2e9c7e46ce6f079331
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2024-09-16 22:54:16 -0300

    refactor: ensure react recognizes isLogged on a DOM

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import isPropValid from '@emotion/is-prop-valid'
 
 export const StyledHeader = styled.header`
   background-color: navy;
@@ -29,7 +30,9 @@ interface AuthButtonProps {
   isLogged: boolean;
 }
 
-export const AuthButton = styled.button<AuthButtonProps>`
+export const AuthButton = styled.button.withConfig({
+  shouldForwardProp: (prop) => isPropValid(prop) && prop !== 'isLogged',
+})<AuthButtonProps>`
   border:none;
   border-radius: 5px;
   height:30px;
@@ -41,7 +44,7 @@ export const AuthButton = styled.button<AuthButtonProps>`
   align-items: center;
   gap: 0.3rem;
   font-size: 0.8rem;
-`
+`;
 
 export const CartButton = styled.button`border:none;
 border-radius: 5px;
